5c3bf1c20f9834ae691256ecc8f58cdee9db258c
Dmitry <dmitry.litviak@gmail.com>
2013-06-12 13:45:37 +0400

    fb, tw, google

--- application/classes/Controller/Company.php
+++ application/classes/Controller/Company.php
@@ -42,7 +42,7 @@ class Controller_Company extends My_Layout_User_Controller {
 //            Helper_Main::print_flex($_SESSION);die;
             $this->redirect("company/info/" . $_SESSION['info']);
         } catch (Exception $e) {
-            echo "Ooophs, we got an error: " . $e->getMessage();
+            Helper_Main::print_flex($e->getMessage());
         }
     }
     
